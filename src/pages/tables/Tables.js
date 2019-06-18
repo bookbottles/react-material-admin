8de1060a615dd2cdfa06719d48226838a52bee7b
Patrick Huber <patrick.huber@vemos.io>
2019-06-18 15:45:17 -0500

    Initial UI mockup of new transactions page using mui-datatables component

--- src/pages/tables/Tables.js
+++ src/pages/tables/Tables.js
@@ -1,50 +1,193 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import DateRangeIcon from '@material-ui/icons/DateRange';
 import MUIDataTable from "mui-datatables";
 
 import PageTitle from '../../components/PageTitle';
-import Widget from '../../components/Widget';
-import Table from '../dashboard/components/Table/Table';
-import mock from '../dashboard/mock'; 
+import Widget from "../../components/Widget";
+
+import { Typography } from "../../components/Wrappers";
 
 const datatableData = [
- ["Joe James", "Example Inc.", "Yonkers", "NY"],
- ["John Walsh", "Example Inc.", "Hartford", "CT"],
- ["Bob Herm", "Example Inc.", "Tampa", "FL"],
- ["James Houston", "Example Inc.", "Dallas", "TX"],
- ["Prabhakar Linwood", "Example Inc.", "Hartford", "CT"],
- ["Kaui Ignace", "Example Inc.", "Yonkers", "NY"],
- ["Esperanza Susanne", "Example Inc.", "Hartford", "CT"],
- ["Christian Birgitte", "Example Inc.", "Tampa", "FL"],
- ["Meral Elias", "Example Inc.", "Hartford", "CT"],
- ["Deep Pau", "Example Inc.", "Yonkers", "NY"],
- ["Sebastiana Hani", "Example Inc.", "Dallas", "TX"],
- ["Marciano Oihana", "Example Inc.", "Yonkers", "NY"],
- ["Brigid Ankur", "Example Inc.", "Dallas", "TX"],
- ["Anna Siranush", "Example Inc.", "Yonkers", "NY"],
- ["Avram Sylva", "Example Inc.", "Hartford", "CT"],
- ["Serafima Babatunde", "Example Inc.", "Tampa", "FL"],
- ["Gaston Festus", "Example Inc.", "Tampa", "FL"],
+ ["2019/02/14 11:50PM", "Joe James", "POS", "$10.00", "Cash", "John Referrer", "Jane Server", "", "Bar", "Refund"],
+ ["2019/02/14 10:33PM", "John Walsh", "POS", "$10.00", "Credit", "John Referrer", "Jane Server", "", "Bar", "Refund"],
+ ["2019/02/14 10:01PM", "", "General Admission", "$10.00", "Credit", "John Referrer", "", "", "Bar", "Refund"],
+ ["2019/02/10 8:00AM", "James Houston", "Ticket", "$10.00", "Credit", "John Referrer", "", "10OFF", "Bar", "Refund"],
+ ["2019/02/09 1:54AM", "Prabhakar Linwood", "POS", "$10.00", "Credit", "John Referrer", "Jane Server", "", "Bar", "Refund"],
+ ["2019/02/09 1:02AM", "Kaui Ignace", "POS", "$10.00", "Cash", "John Referrer", "Jane Server", "", "Bar", "Refund"],
+ ["2019/02/09 10:09PM", "Esperanza Susanne", "POS", "$10.00", "Cash", "John Referrer", "Jane Server", "", "Bar", "Refund"],
+ ["2019/01/31 10:00PM", "", "General Admission", "$10.00", "Cash", "John Referrer", "", "", "Bar", "Refund"],
+ ["2019/01/31 10:00PM", "Meral Elias", "Guest List", "$10.00", "Comped", "John Referrer", "", "", "Bar", "Refund"],
+ ["2019/01/31 10:00PM", "Deep Pau", "Guest List", "$10.00", "Cash", "John Referrer", "", "", "Bar", "Refund"],
+ ["2019/01/31 10:00PM", "Sebastiana Hani", "POS", "$10.00", "Credit", "John Referrer", "Jane Server", "", "Bar", "Refund"],
+ ["2019/01/31 10:00PM", "Marciano Oihana", "POS", "$10.00", "Credit", "John Referrer", "Jane Server", "", "Bar", "Refund"],
+ ["2019/01/11 10:00PM", "Brigid Ankur", "Ticket", "$10.00", "Credit", "John Referrer", "", "", "Bar", "Refund"],
+ ["2019/01/11 10:00PM", "Anna Siranush", "Ticket", "$10.00", "Cash", "John Referrer", "", "", "Bar", "Refund"],
+ ["2019/01/11 10:00PM", "Avram Sylva", "Ticket", "$10.00", "Cash", "John Referrer", "", "", "Bar", "Refund"],
+ ["2019/01/10 10:00PM", "Serafima Babatunde", "Ticket", "$10.00", "Cash", "John Referrer", "", "10OFF", "Bar", "Refund"],
+ ["2019/01/01 10:00PM", "Gaston Festus", "POS", "$10.00", "Cash", "John Referrer", "Jane Server", "", "Bar", "Refund"],
 ];
 
 const Tables = props => (
   <React.Fragment>
-    <PageTitle title="Tables" />
+    <Grid container spacing={0}>
+      <Grid item xs={9}>
+        <PageTitle title="React Transactions" />
+      </Grid>
+      <Grid item xs={3}>
+        <Button
+          color="primary"
+          variant="contained"
+          size="small"
+          style={{float: 'right', marginTop: '35px'}}
+        >
+          <DateRangeIcon />&nbsp;
+          Jan 1, 2019 - March 1, 2019
+          <ArrowDropDownIcon />
+        </Button>
+      </Grid>
+    </Grid>
+    <Grid container spacing={32}>
+        <Grid item lg={3} md={4} sm={6} xs={12}>
+          <Widget
+            title="Total Sales"
+            upperTitle
+            disableWidgetMenu
+          >
+            <Typography size="xl" weight="medium">
+              $12,678.00
+            </Typography>
+          </Widget>
+      </Grid>
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+          <Widget
+            title="Cash Sales"
+            upperTitle
+            disableWidgetMenu
+          >
+            <Typography size="xl" weight="medium">
+              $2,678.00
+            </Typography>
+          </Widget>
+      </Grid>
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+          <Widget
+            title="Credit Sales"
+            upperTitle
+            disableWidgetMenu
+          >
+            <Typography size="xl" weight="medium">
+              $10,000.00
+            </Typography>
+          </Widget>
+      </Grid>
+      <Grid item lg={3} md={4} sm={6} xs={12}>
+          <Widget
+            title="Transactions"
+            upperTitle
+            disableWidgetMenu
+          >
+            <Typography size="xl" weight="medium">
+              128
+            </Typography>
+          </Widget>
+      </Grid>
+    </Grid>
     <Grid container spacing={32}>
       <Grid item xs={12}>
         <MUIDataTable
-          title="Employee List"
+          title="Transactions"
           data={datatableData}
-          columns={["Name", "Company", "City", "State"]}
+          columns={[
+            {
+              name: "Date",
+              options: {
+                filter: false,
+                sort: true
+              }
+            },
+            {
+              name: "Guest",
+              options: {
+                filter: true,
+                filterType: "textField",
+                sort: true
+              }
+            },
+            {
+              name: "Type",
+              options: {
+                filter: true,
+                filterType: "multiselect",
+                sort: true
+              }
+            },
+            {
+              name: "Amount",
+              options: {
+                filter: false,
+                sort: true
+              }
+            },
+            {
+              name: "Payment Method",
+              options: {
+                filter: true,
+                filterType: "multiselect",
+                display: false,
+                sort: true
+              }
+            },
+            {
+              name: "Referrer",
+              options: {
+                filter: true,
+                filterType: "textField",
+                display: false,
+                sort: true
+              }
+            },
+            {
+              name: "Server",
+              options: {
+                filter: true,
+                display: false,
+                sort: true
+              }
+            },
+            {
+              name: "Promo Code",
+              options: {
+                filter: true,
+                display: false,
+                sort: true
+              }
+            },
+            {
+              name: "Revenue Center",
+              options: {
+                filter: true,
+                display: false,
+                sort: true
+              }
+            },
+            {
+              name: "Refund",
+              options: {
+                filter: true,
+                display: false,
+                sort: true
+              }
+            },
+          ]}
           options={{
-            filterType: 'checkbox',
+            selectableRows: false,
+            expandableRows: true,
+            expandableRowsOnClick: true,
           }}
         />
-      </Grid>
-      <Grid item xs={12}>
-        <Widget title="Material-UI Table" upperTitle noBodyPadding>
-          <Table data={mock.table} />
-        </Widget>
       </Grid>
     </Grid>
   </React.Fragment>
